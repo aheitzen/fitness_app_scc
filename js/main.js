7f06e76fe592a6cdbeefaa141075ede5594bfd52
Alexa Heitzenrader <alexaheitzenrader@Alexas-MBP.PK5001Z>
2017-11-07 21:53:31 -0800

    working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-console.log("I work");
+var currentVideo = 0
+var videos = [
+	{
+		src: 'video/pullUp.mp4',
+		type: 'video/mp4'
+	},
+		{
+		src: 'video/Workout.mp4',
+		type: 'video/mp4'
+	}
+]
+
+var videoElement = $('video')
+var sourceElement = $('video source')
+
+videoElement.on('ended',function(){
+	sourceElement.attr('src', videos[currentVideo].src)
+	sourceElement.attr('type', videos[currentVideo].type)
+	videoElement.get(0).load();
+	videoElement[0].play();
+	
+	if (currentVideo >= videos.length - 1) {
+		currentVideo = 0;
+	} else {
+		currentVideo++;
+	}
+});
